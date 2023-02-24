850d7790f8803e0922707f8c4129639d6e294d2d
atomjoy <atomjoy.official@gmail.com>
2023-02-24 16:30:32 +0100

    Change locale Vue3 Options Api
    Change locale Vue 3 Options Api.

---
+++ ChangeLocaleOptionsApi.vue
@@ -0,0 +1,52 @@
+<template>
+	<div class="locale-changer">
+		<select v-model="$i18n.locale" class="locale-changer-select">
+			<option v-for="locale in $i18n.availableLocales" :key="`locale-${locale}`" :value="locale">{{ locale }}</option>
+		</select>
+	</div>
+</template>
+
+<script>
+// import { mapActions } from 'pinia'
+// import { useAuthStore } from '@/stores/auth.js'
+
+export default {
+	name: 'ChangeLocale',
+	data() {
+		return {}
+	},
+	methods: {
+		// ...mapActions(useAuthStore, ['changeLocale']),
+	},
+	watch: {
+		// Change server locale
+		'$i18n.locale': function (newVal, oldVal) {
+			console.log('From ', oldVal, 'to', newVal, 'curr', this.$i18n.locale)
+      // Refresh server locale
+			// this.changeServerLocale(newVal)
+		},
+	},
+}
+</script>
+
+<style scoped>
+.locale-changer {
+	float: right;
+	width: auto;
+	height: auto;
+	padding: 5px;
+}
+.locale-changer-select {
+	float: right;
+	width: 50px;
+	padding: 2px;
+	text-align: center;
+	border: 0px;
+	background: transparent;
+	font-size: 15px;
+}
+.locale-changer-select:focus {
+	border: none;
+	box-shadow: none;
+}
+</style>
